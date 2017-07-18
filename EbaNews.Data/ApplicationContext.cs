a948be8f9013215fffff362dc0cf4d4e64355d4d
SlavBro <iamslavon@gmail.com>
2017-07-19 00:48:16 +0300

    Changed table names in db

--- EbaNews.Data/ApplicationContext.cs
+++ EbaNews.Data/ApplicationContext.cs
@@ -1,5 +1,6 @@
 ﻿using EbaNews.Core.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
 
 namespace EbaNews.Data
 {
@@ -10,6 +11,21 @@ namespace EbaNews.Data
         public static ApplicationContext Create()
         {
             return new ApplicationContext();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<ApplicationUser>()
+                    .ToTable("Users", "dbo");
+            modelBuilder.Entity<IdentityRole>()
+                    .ToTable("Roles", "dbo");
+            modelBuilder.Entity<IdentityUserLogin>()
+                    .ToTable("Logins", "dbo");
+            modelBuilder.Entity<IdentityUserClaim>()
+                    .ToTable("Claims", "dbo");
+            modelBuilder.Entity<IdentityUserRole>()
+                    .ToTable("UserRoles", "dbo");
         }
 
         //public DbSet<Person> People { get; set; }
