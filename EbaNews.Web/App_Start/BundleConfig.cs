2dcb6a516c3cc68650c269fedd3b6822c219ab99
SlavBro <iamslavon@gmail.com>
2017-10-01 16:38:53 +0300

    Refactored js

--- EbaNews.Web/App_Start/BundleConfig.cs
+++ EbaNews.Web/App_Start/BundleConfig.cs
@@ -11,26 +11,18 @@ namespace EbaNews.Web
 #else
             BundleTable.EnableOptimizations = true;
 #endif
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/libs/admin").Include(
+                        "~/Scripts/Libs/jquery-3.1.1.min.js",
+                        "~/Scripts/Libs/moment.min.js",
+                        "~/Scripts/Libs/angular.min.js",
+                        "~/Scripts/Libs/modernizr-*",
+                        "~/Scripts/Libs/bootstrap.min.js",
+                        "~/Scripts/Libs/respond.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/moment").Include(
-                        "~/Scripts/moment.min.js"));
+            bundles.Add(new ScriptBundle("~/app/admin").Include(
+                        "~/Scripts/Areas/Admin/app.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
-                        "~/Scripts/angular.min.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.min.js",
-                      "~/Scripts/respond.js"));
-
-            bundles.Add(new StyleBundle("~/Content/admin-css").Include(
+            bundles.Add(new StyleBundle("~/css/admin").Include(
                       "~/Content/bootstrap.min.css",
                       "~/Content/font-awesome.min.css",
                       "~/Content/Admin.css",
