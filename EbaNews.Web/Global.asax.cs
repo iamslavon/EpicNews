4088f701dfca1f32de1e785ef500d3bc98fcecdc
SlavBro <iamslavon@gmail.com>
2017-10-09 00:16:18 +0300

    Added logging

--- EbaNews.Web/Global.asax.cs
+++ EbaNews.Web/Global.asax.cs
@@ -1,5 +1,6 @@
 ﻿using EbaNews.Ioc;
 using EbaNews.Web.Mapping;
+using NLog;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -8,6 +9,8 @@ namespace EbaNews.Web
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        private readonly Logger logger = LogManager.GetCurrentClassLogger();
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
@@ -16,6 +19,12 @@ namespace EbaNews.Web
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             IocContainer.Initialize();
             AutoMapperConfig.Register();
+        }
+
+        protected void Application_Error()
+        {
+            var exception = Server.GetLastError();
+            logger.Error(exception);
         }
     }
 }
