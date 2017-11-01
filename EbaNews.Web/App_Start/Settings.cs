c61b2a92c3e1041d648a529b15650e6fb8a7411d
SlavBro <iamslavon@gmail.com>
2017-11-02 00:41:20 +0200

    Project basic localization logic

--- EbaNews.Web/App_Start/Settings.cs
+++ EbaNews.Web/App_Start/Settings.cs
@@ -4,11 +4,17 @@ namespace EbaNews.Web
 {
     public static class Settings
     {
-        public static int AllowedNewsCount { get; set; }
-
         public static void Init()
         {
             AllowedNewsCount = Convert.ToInt32(System.Configuration.ConfigurationManager.AppSettings["AllowedNewsCount"]);
+            DefaultCulture = System.Configuration.ConfigurationManager.AppSettings["DefaultCulture"];
+            CookieName = System.Configuration.ConfigurationManager.AppSettings["CookieName"];
         }
+
+        public static int AllowedNewsCount { get; set; }
+
+        public static string DefaultCulture { get; set; }
+
+        public static string CookieName { get; set; }
     }
 }
--- EbaNews.Web/Global.asax.cs
+++ EbaNews.Web/Global.asax.cs
@@ -1,13 +1,16 @@
 ﻿using EbaNews.Ioc;
 using EbaNews.Web.Mapping;
 using NLog;
+using System.Globalization;
+using System.Threading;
+using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace EbaNews.Web
 {
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : HttpApplication
     {
         private readonly Logger logger = LogManager.GetCurrentClassLogger();
 
@@ -20,6 +23,31 @@ namespace EbaNews.Web
             IocContainer.Initialize();
             AutoMapperConfig.Register();
             Settings.Init();
+        }
+
+        protected void Application_BeginRequest()
+        {
+            var culture = Request.Cookies[$"{Settings.CookieName}_culture"]?.Value;
+
+            if (culture == null)
+            {
+                var browserLanguages = Request.UserLanguages;
+
+                if (browserLanguages != null && browserLanguages.Length > 0)
+                {
+                    culture = browserLanguages[0].Substring(0, 2);
+                }
+                else
+                {
+                    culture = Settings.DefaultCulture;
+                }
+
+                Response.SetCookie(new HttpCookie($"{Settings.CookieName}_culture", culture));
+            }
+
+            var cultureInfo = new CultureInfo(culture);
+            Thread.CurrentThread.CurrentCulture = cultureInfo;
+            Thread.CurrentThread.CurrentUICulture = cultureInfo;
         }
 
         protected void Application_Error()
