b6f386bfc59098d14cf89bc83dd91084fda11abd
SlavBro <iamslavon@gmail.com>
2017-11-12 23:14:31 +0200

    Copy to clipboard button now works good

--- EbaNews.Web/Scripts/Areas/Home/Shared/newsItem.js
+++ EbaNews.Web/Scripts/Areas/Home/Shared/newsItem.js
@@ -1,7 +1,17 @@
-﻿angular.module("newsItem", [])
-    .controller("newsItemController", ["$scope", function ($scope) {
-        
-    }])
+﻿angular.module("newsItem", ["ngNotify", "angular-clipboard"])
+    .controller("newsItemController", function ($scope, clipboard, ngNotify) {
+
+        $scope.showCopyButton = function () {
+            return clipboard.supported;
+        };
+
+        $scope.copyToClipboard = function (id) {
+            var link = window.location.origin + "/news/" + id;
+            clipboard.copyText(link);
+            ngNotify.set("Link was copied to clipboard", "success");
+        };
+
+    })
     .directive("newsItem", function () {
         return {
             restrict: "E",
